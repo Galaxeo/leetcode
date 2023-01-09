efc15e7714159c9ab6fcc33d4e052b5bae63eb2c
Justin C <hwjustincheok@gmail.com>
2023-01-08 19:40:59 -0800

    Python solution w/ explanation (beats 99.57% runtime)

--- lettercombsphonenum.py
+++ lettercombsphonenum.py
@@ -2,7 +2,14 @@
 Runtime: beats 99.57%
 Memory: Beats 77.32%
 
-A more simple method for this would be to utilize 
+A more simple method for this would be to utilize nested 
+for loops to create each combination. This wouldn't require
+you to check if the letters were in the correct spots.
+
+I wanted to use itertools for a problem, so I tried it out here
+and it ended up working out. You can further optimize the runtime
+with different methods like maps or maybe even for-loops, but
+itertools combinations was a very easy implementation.
 """
 
 import itertools
@@ -20,6 +27,7 @@ letternum = {
 def main(digits):
     if digits == "":
         return []
+    
     def validity(comb):
         i = 0
         while i != len(digits):
@@ -35,8 +43,8 @@ def main(digits):
                 continue 
             letters += letternum[int(digits[i])]
         return letters
-    lst = [''.join(x) for x in itertools.combinations(getletters(digits), len(digits)) if validity(x)]
-    lst = [*set(lst)]
+    lst = [*set([''.join(x) for x in itertools.combinations(getletters(digits), len(digits)) if validity(x)])]
     return lst
+
 for digits in lstofdigits:
     print(main(digits))
